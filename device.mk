a24935bad0bde24b49f014a77c50ec677ff0694a
Jamie Falconer <mrfluffyoven@gmail.com>
2024-07-29 10:09:09 +0100

    add vendor modules

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     libdmabufheap \
     libion \
+    libxml2 \
     libnetutils \
     vendor.display.config@1.0 \
     vendor.display.config@2.0 \
@@ -41,6 +42,7 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libdmabufheap.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libnetutils.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so 
