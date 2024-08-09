c1e182fbfb150d6360dfe1e366136128eb09f457
Jamie Falconer <mrfluffyoven@gmail.com>
2024-08-09 10:10:38 +0100

    REVERT: manifest & ueventd.rc

--- device.mk
+++ device.mk
@@ -18,7 +18,13 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd 
 
-#PRODUCT_PACKAGES += \
+# BOOT
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
     otapreopt_script \
     update_engine \
     update_engine_sideload \
@@ -48,7 +54,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so 
 
 # QCOM
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
