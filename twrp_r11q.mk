d8b32cff90f09714c4bb37ad43d7dac80af93026
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-08-16 11:51:57 +0100

    remove this, thanks @cd-Crypton

--- twrp_r11q.mk
+++ twrp_r11q.mk
@@ -21,8 +21,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/$(PRODUCT_RELEASE_NAME)/device.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/$(PRODUCT_RELEASE_NAME)/recovery/root,recovery/root)
-
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
 PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
 PRODUCT_BRAND := samsung
